4a8233993596884c2451edd7be92a3c6071bde73
Zander Trevino <xboyzander@gmail.com>
2023-09-04 12:17:06 -0700

    Third Commit: Switch Statement Added

--- main.c
+++ main.c
@@ -9,6 +9,46 @@ int main(void) {
     // Don't worry about the `scanf()` below, you'll learn more about these later!
     scanf("%d /%d /%d", &month, &day, &year);
 
+
+    switch (month)
+    {
+        case 1:
+            printf("January");
+            break;
+        case 2:
+            printf("February");
+            break;
+        case 3:
+            printf("March");
+            break;
+        case 4:
+            printf("April");
+            break;
+        case 5:
+            printf("May");
+            break;
+        case 6:
+            printf("June");
+            break;
+        case 7:
+            printf("July");
+            break;
+        case 8:
+            printf("August");
+            break;
+        case 9:
+            printf("September");
+            break;
+        case 10:
+            printf("October");
+            break;
+        case 11:
+            printf("November");
+            break;
+        case 12:
+            printf("December");
+            break;
+    }
     // Print the month
 
 
